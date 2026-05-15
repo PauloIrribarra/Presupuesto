189966d118170ef52faaaa157361164fc26d70af
Paulo Irribarra <paulo.irribarra18@gmail.com>
2026-05-15 01:35:42 -0400

    Configurar deploy en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	base: '/Presupuesto/',
 	plugins: [react()],
+	base: '/Presupuesto/',
 });
